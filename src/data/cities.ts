c11ebcf6b6490152a63fc3c4634126853a62f801
gene george <genewgeorge@gmail.com>
2026-04-08 06:37:31 -0400

    Update cities.ts

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -1,12 +1,12 @@
 export const cities = [
-  "Amelia", "Powhatan", "Hanover", "Richmond", "Henrico", 
-  "Chesterfield", "Chester", "Midlothian", "Glen Allen", 
-  "Short Pump", "Mechanicsville", "Ashland", "Petersburg", 
-  "Hopewell", "Colonial Heights", "Dinwiddie", "Prince George", 
-  "Charles City", "New Kent", "Goochland", "Louisa", 
-  "Cumberland", "Buckingham", "Appomattox", "Farmville", 
-  "Blackstone", "Kenbridge", "Victoria", "South Hill", 
-  "Chase City", "Clarksville", "Lawrenceville", "Emporia", 
-  "Franklin", "Suffolk", "Smithfield", "Isle of Wight", 
-  "Surry", "Sussex", "Waverly", "Wakefield"
+  "amelia", "powhatan", "hanover", "richmond", "henrico", 
+  "chesterfield", "chester", "midlothian", "glen-allen", 
+  "short-pump", "mechanicsville", "ashland", "petersburg", 
+  "hopewell", "colonial-heights", "dinwiddie", "prince-george", 
+  "charles-city", "new-kent", "goochland", "louisa", 
+  "cumberland", "buckingham", "appomattox", "farmville", 
+  "blackstone", "kenbridge", "victoria", "south-hill", 
+  "chase-city", "clarksville", "lawrenceville", "emporia", 
+  "franklin", "suffolk", "smithfield", "isle-of-wight", 
+  "surry", "sussex", "waverly", "wakefield"
 ];
