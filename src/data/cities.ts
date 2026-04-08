e45ebb56d865ed9b9d70b438931d021ff0d31e3b
gene george <genewgeorge@gmail.com>
2026-04-08 06:13:34 -0400

    Create cities.ts

---
+++ src/data/cities.ts
@@ -0,0 +1,12 @@
+export const cities = [
+  "Amelia", "Powhatan", "Hanover", "Richmond", "Henrico", 
+  "Chesterfield", "Chester", "Midlothian", "Glen Allen", 
+  "Short Pump", "Mechanicsville", "Ashland", "Petersburg", 
+  "Hopewell", "Colonial Heights", "Dinwiddie", "Prince George", 
+  "Charles City", "New Kent", "Goochland", "Louisa", 
+  "Cumberland", "Buckingham", "Appomattox", "Farmville", 
+  "Blackstone", "Kenbridge", "Victoria", "South Hill", 
+  "Chase City", "Clarksville", "Lawrenceville", "Emporia", 
+  "Franklin", "Suffolk", "Smithfield", "Isle of Wight", 
+  "Surry", "Sussex", "Waverly", "Wakefield"
+];
